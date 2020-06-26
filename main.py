33457dbb9b844aa09b345aca8144683b8dff7be3
Nataly <rnataly@google.com>
2020-06-26 12:47:03 -0400

    comments and title

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
 
+# initializes the flask app
 app = Flask(__name__)
 
 @app.route("/")
 def homepage():
+    """"Renders the index page of the app."""
     return render_template("index.html")
 
+# runs the flask app
 if __name__ == "__main__":
     app.run(debug=True)
