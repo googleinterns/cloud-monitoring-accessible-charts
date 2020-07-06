8e3fe4234445d04261d0a522763c5b59af7431fb
Nataly <rnataly@google.com>
2020-07-06 14:14:55 -0400

    initial front-end

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import json
 
 # initializes the flask app
 app = Flask(__name__)
@@ -7,6 +8,13 @@ app = Flask(__name__)
 def homepage():
     """"Renders the index page of the app."""
     return render_template("index.html")
+
+@app.route("/data/<file_name>")
+def send_data(file_name):
+    """Sends the requested file."""
+    with open('./'+str(file_name),"r") as json_file:
+        data = json.load(json_file)
+    return data
 
 # runs the flask app
 if __name__ == "__main__":
